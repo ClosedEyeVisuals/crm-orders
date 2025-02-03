346fdef6c8edbc1e5d90c9375d43b6bb22717050
Bogdan Komandirov <closedeyevisuals@yandex.ru>
2025-02-03 20:04:47 +0700

    add 'create' url

--- crm/orders/urls.py
+++ crm/orders/urls.py
@@ -8,6 +8,7 @@ app_name = 'orders'
 urlpatterns = [
     path('', views.HomePage.as_view(), name='index'),
     path('orders/', views.OrderListView.as_view(), name='list'),
+    path('orders/create/', views.OrderCreateView.as_view(), name='create'),
     path(
         'orders/<int:order_id>/edit/',
         views.OrderUpdateView.as_view(),
