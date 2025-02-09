f0774357f93be0b57015e03803bfdeca34053aae
Bogdan Komandirov <closedeyevisuals@yandex.ru>
2025-02-09 23:40:52 +0700

    add date range filter

--- crm/orders/filters.py
+++ crm/orders/filters.py
@@ -1,12 +1,30 @@
-from django_filters import FilterSet
+import django_filters
 
 from orders.models import Order
 
 
-class OrderFilter(FilterSet):
+class OrderFilter(django_filters.FilterSet):
     class Meta:
         model = Order
         fields = (
             'table_number',
-            'status',
+            'status'
+        )
+
+
+class OrderDateRangeFilter(django_filters.FilterSet):
+    created_at = django_filters.DateFromToRangeFilter(
+        label='Период',
+        widget=django_filters.widgets.RangeWidget(
+            attrs={
+                'class': 'datepicker',
+                'type': 'date'
+            }
+        )
+    )
+
+    class Meta:
+        model = Order
+        fields = (
+            'created_at',
         )
